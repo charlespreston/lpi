3bacd782384672ddf3ce744e1d935f60ed819ab3
Charles Preston <charlespreston@yahoo.com>
2014-07-01 12:33:28 -0700

    commit for coursera crypto101

---
+++ cf/problem2.py
@@ -0,0 +1,23 @@
+import sys
+
+MSGS = ("message 1", "message 2", "message 3", "message 4", "message 5", "message 6", "message 7", "message 8", "message 9", "message 10", "message 11")
+
+def strxor(a, b):     # xor two strings of different lengths
+    if len(a) > len(b):
+        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
+    else:
+        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
+
+def random(size=16):
+    return open("/dev/urandom").read(size)
+
+def encrypt(key, msg):
+    c = strxor(key, msg)
+    print
+    print c.encode('hex')
+    return c
+
+def main():
+    key = random(1024)
+    ciphertexts = [encrypt(key, msg) for msg in MSGS]
+    print ciphertexts
